b021465c810193aa8596f7bcba8ddeb0730a25c2
apetit2 <apetit@wpi.edu>
2017-09-05 12:56:04 -0400

    dfs limit and iterative both work

--- src/main/SearchMethods.java
+++ src/main/SearchMethods.java
@@ -43,11 +43,26 @@ public class SearchMethods {
             }
     }
     
-    public static void dfsLimit(Node start, ArrayList<Node> nodes, boolean[] v, ArrayList<ArrayList<Node>> toPrint, int limit, int at){
-        System.out.println(start.getNodeName() + "\t\t" + toPrint);
-        int index = 0;
-        ArrayList<ArrayList<Node>> tmp = new ArrayList<>();
-        if(at < limit){
+    public static Node dfsLimit(Node start, ArrayList<Node> nodes, boolean[] v, ArrayList<ArrayList<Node>> toPrint, int limit){       
+        System.out.println(start + "\t\t" + toPrint);
+        if(limit < 0){
+            System.out.println("Limit was below 0");
+            return null;
+        }
+        if((limit == 0) && (start.getNodeName().equals("G"))){
+            return start;
+        }
+        if(limit == 0){
+            if(!toPrint.isEmpty()){
+                toPrint.remove(0);
+            }
+        }
+        if(start.getNodeName().equals("S")){
+            v[start.getIndex()] = true;
+        }
+        if (limit > 0){
+            int index = 0;
+            ArrayList<ArrayList<Node>> tmp = new ArrayList<>();
             while(index < start.getNeighbors().size()){
                 ArrayList<Node> t = new ArrayList(toPrint.get(0));
                 if(v[start.getNeighbors().get(index).getIndex()] != true){
@@ -60,28 +75,40 @@ public class SearchMethods {
             for(int i = tmp.size() - 1; i >= 0; i--){
                 toPrint.add(0, tmp.get(i));
             }
-            v[start.getIndex()] = true;
-        } else {
-            toPrint.remove(0);
-        }
-        for(Node node : nodes.get(start.getIndex()).getNeighbors()){
-            if(at < limit){
-                if(!(v[node.getIndex()] == true)){
-                
-                    if(node.getNodeName().equals("G")){
-                        System.out.println(node.getNodeName() + "\t\t" + toPrint);
-                        System.out.println("Goal Reached!");
-                        break;
-                    } 
-                
-                    dfsLimit(node, nodes, v, toPrint, limit, at + 1);
+            for (Node node : start.getNeighbors()){
+                boolean[] newVisited = new boolean[nodes.size()];
+                for (int i = 0; i < v.length; i++){
+                    newVisited[i] = v[i];
+                }
+                if(newVisited[node.getIndex()] == false){
+                    newVisited[node.getIndex()] = true;
+                    Node found = dfsLimit(node, nodes, newVisited, toPrint, limit - 1);
+                    if (found != null){
+                        return found;
+                    }
                 }
             }
         }
+        return null;
     }
     
-    public static void iterativeDeeping(Node start, ArrayList<Node> nodes, boolean[] v, ArrayList<ArrayList<Node>> toPrint){
-        
+    public static void iterativeDeeping(Node start, ArrayList<Node> nodes, ArrayList<ArrayList<Node>> toPrint){
+        Node check = null;
+        int index = 0;
+        while(check == null){ 
+            boolean[] v = new boolean[nodes.size()];
+            toPrint = new ArrayList<>();
+            ArrayList<Node> tmp = new ArrayList<>();
+            tmp.add(start);
+            toPrint.add(tmp);
+            System.out.println("L = " + index);
+            System.out.println("Expanded \tQueue");
+            check = SearchMethods.dfsLimit(start, nodes, v, toPrint, index);
+            index++;
+            if (check == null){
+                System.out.println("\n");
+            }
+        }
     }
     
     public static void bfs(Node start, ArrayList<Node> nodes, boolean[] v, ArrayList<ArrayList<Node>> toPrint){
@@ -100,6 +127,10 @@ public class SearchMethods {
             }
         }
        
+    }
+    
+    public static void uniformSearch(Node start, ArrayList<Node> nodes, ArrayList<ArrayList<Node>> toPrint){
+        
     }
     
 }
