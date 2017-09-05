b6c7397b6ecb531bfb81deb98a9099ac45996da9
apetit2 <apetit@wpi.edu>
2017-09-05 13:28:55 -0400

    added comments

--- src/main/MainDriver.java
+++ src/main/MainDriver.java
@@ -46,13 +46,19 @@ public class MainDriver {
             //set up the graph
             int index = 0;
             for(String string : strings){
+                //break the string up into 3 arguments (nodeName, nodeName, and distance)
                 String[] str = string.split("\\s+");
                 Node start = new Node(str[0]);
                 Node end = new Node(str[1]);
                 double distance = Double.parseDouble(str[2]);
+                
+                //these are used to mark the index that node shows up in the initial array
+                //we will use this later for a visited array
                 int startExists = -1, endExists = -1;
+                
+                //if nodes is not empty we should update the existing nodes if the start or end exist
                 if(!nodes.isEmpty()){
-                    for (int i = 0; i < nodes.size(); i++){
+                    for (int i = 0; i < nodes.size(); i++){ //find the start/end nodes for the given pair and update them if they exist
                         if (nodes.get(i).getNodeName().equals(start.getNodeName())){
                             start.setNeighbors(nodes.get(i).getNeighbors());
                             start.setLinks(nodes.get(i).getLinks());
@@ -64,45 +70,50 @@ public class MainDriver {
                             endExists = i;
                         }     
                     }
+                    //this index value is used to determine where the node is in the nodes array
                     index = nodes.size();
                     
-                    if(startExists == -1){
+                    if(startExists == -1){ //start node does not initially exist so it will be at the next position in the array
                         start.setIndex(index);
                         index++;
-                    } else if(startExists >= 0){
+                    } else if(startExists >= 0){ //start node does exist so we must keep the index at the same position
                         start.setIndex(startExists);
                     }
                     
-                    if(endExists == -1){
+                    if(endExists == -1){ //same thing as above but for end node
                         end.setIndex(index);
                     } else if(endExists >= 0){
                         end.setIndex(endExists);
                     }
                     
+                    //add the end node to the start nodes neighbors
                     start.addNeighbor(end);
-                    start.addLink(distance, end);
+                    start.addLink(distance, end); //set distance
                     
+                    //add the start node to the end node neighbors
                     end.addNeighbor(start);
-                    end.addLink(distance, start);
+                    end.addLink(distance, start); //set distance
                     
-                    if(startExists == -1){
+                    if(startExists == -1){ //if start does not exist add it to the nodes array
                         nodes.add(start);
-                    } else if (startExists >= 0){
+                    } else if (startExists >= 0){ //else just update the existing node
                         nodes.get(startExists).setLinks(start.getLinks());
-                        Collections.sort(start.getNeighbors(), new NameComparator());
+                        //sort the neighbor nodes so that the first one is the first to appear in the alphabet and so forth
+                        Collections.sort(start.getNeighbors(), new NameComparator()); 
                         nodes.get(startExists).setNeighbors(start.getNeighbors());
                         
                     }
-                    if(endExists == -1){
+                    if(endExists == -1){ //same thing just for end node
                         end.setIndex(index);
                         nodes.add(end);
                         index++;
-                    } else if (endExists >= 0){
+                    } else if (endExists >= 0){ //...
                         nodes.get(endExists).setLinks(end.getLinks());
+                        //sort the neighbor nodes so that the first one is the first to appear in the alphabet and so forth
                         Collections.sort(end.getNeighbors(), new NameComparator());
                         nodes.get(endExists).setNeighbors(end.getNeighbors());
                     }
-                } else {
+                } else { //nodes array has nothing in it so add the nodes to it
                     
                     start.setIndex(index);
                     index++;
@@ -122,7 +133,7 @@ public class MainDriver {
                 }
             }
             
-            //get the start node
+            //get the start node so we know where to begin
             Node start = null;
             for(int i = 0; i < nodes.size(); i++){
                 if(nodes.get(i).getNodeName().equals("S")){
@@ -174,21 +185,37 @@ public class MainDriver {
             //Uniform Search
             System.out.println("Uniform Search");
             System.out.println("Expanded \tQueue");
+            array = new boolean[nodes.size()];
+            toPrint = new ArrayList<>();
+            toPrint.add(tmp);
+            SearchMethods.uniformSearch(start, nodes, array, toPrint);
             System.out.println("\n\n");
             
             //Greedy Search
             System.out.println("Greedy Search");
             System.out.println("Expanded \tQueue");
+            array = new boolean[nodes.size()];
+            toPrint = new ArrayList<>();
+            toPrint.add(tmp);
+            SearchMethods.greedySearch(start, nodes, array, toPrint);
             System.out.println("\n\n");
             
             //A*
             System.out.println("A* Search");
             System.out.println("Expanded \tQueue");
+            array = new boolean[nodes.size()];
+            toPrint = new ArrayList<>();
+            toPrint.add(tmp);
+            SearchMethods.aStar(start, nodes, array, toPrint);
             System.out.println("\n\n");
             
             //Beam Search
             System.out.println("Beam Search");
             System.out.println("Expanded \tQueue");
+            array = new boolean[nodes.size()];
+            toPrint = new ArrayList<>();
+            toPrint.add(tmp);
+            SearchMethods.beamSearch(start, nodes, array, toPrint);
             System.out.println("\n\n");
             
         } catch (IOException e){
