b78fb25b1e9a3e6c202c7fc9f376b477ba6f0768
apetit2 <apetit@wpi.edu>
2017-09-05 13:28:41 -0400

    added comments

--- src/main/SearchMethods.java
+++ src/main/SearchMethods.java
@@ -16,51 +16,66 @@ import java.util.Queue;
 public class SearchMethods {
     public static void dfs(Node start, ArrayList<Node> nodes, boolean[] v, ArrayList<ArrayList<Node>> toPrint){
             System.out.println(start.getNodeName() + "\t\t" + toPrint);
+            
+            //this code is necessary to create the queue to print, this code works for the next node we will visit
             int index = 0;
             ArrayList<ArrayList<Node>> tmp = new ArrayList<>();
-            while(index < start.getNeighbors().size()){
+            while(index < start.getNeighbors().size()){ //go through all the neighbor nodes of the current node to see which ones will be queued 
                 ArrayList<Node> t = new ArrayList(toPrint.get(0));
+                //only add the node into the queue if we have not already visited it
                 if(v[start.getNeighbors().get(index).getIndex()] != true){
                     t.add(0, start.getNeighbors().get(index));
                     tmp.add(t);
                 } 
                 index++;
             }
-            toPrint.remove(0);
-            for(int i = tmp.size() - 1; i >= 0; i--){
-                toPrint.add(0, tmp.get(i));
+            toPrint.remove(0); //remove the first node because we have expanded it
+            for(int i = tmp.size() - 1; i >= 0; i--){ //go through the list of nodes we created in the prior step
+                toPrint.add(0, tmp.get(i)); //add the node back to the toPrint list 
             }
+            
+            //mark this node as though it was visited
             v[start.getIndex()] = true;
+            //go through all of the neighbor nodes and expand them in order that they appear
             for(Node node : nodes.get(start.getIndex()).getNeighbors()){
+                //only if that node had not been visited do we expand it to look for the goal
                 if(!(v[node.getIndex()] == true)){
-                    if(node.getNodeName().equals("G")){
+                    if(node.getNodeName().equals("G")){ //if the node we expanded is the goal, break the loop and print that we got the goal
                         System.out.println(node.getNodeName() + "\t\t" + toPrint);
                         System.out.println("Goal Reached!");
                         break;
                     }
-                    dfs(node, nodes, v, toPrint);
+                    dfs(node, nodes, v, toPrint); //run the program on the next node
                 }
             }
     }
     
     public static Node dfsLimit(Node start, ArrayList<Node> nodes, boolean[] v, ArrayList<ArrayList<Node>> toPrint, int limit){       
         System.out.println(start + "\t\t" + toPrint);
-        if(limit < 0){
+        if(limit < 0){ //if limit is lower that 0, this program makes no sense, so just break it
             System.out.println("Limit was below 0");
             return null;
         }
+        //if the current node is the goal node, just return it because we have the solution
         if((limit == 0) && (start.getNodeName().equals("G"))){
             return start;
         }
+        //if we hit the farthest place we can go in the tree, we should remove the first element in the queue because we can not expand it
         if(limit == 0){
             if(!toPrint.isEmpty()){
                 toPrint.remove(0);
             }
         }
+        
+        //if this is the start node, we should let the program know that we expanded it
         if(start.getNodeName().equals("S")){
             v[start.getIndex()] = true;
         }
+        
+        //this is the case where we would have to expand neighbor nodes if there are any
         if (limit > 0){
+            
+            //sets up the queue for neighbor nodes - refer to DFS
             int index = 0;
             ArrayList<ArrayList<Node>> tmp = new ArrayList<>();
             while(index < start.getNeighbors().size()){
@@ -75,13 +90,15 @@ public class SearchMethods {
             for(int i = tmp.size() - 1; i >= 0; i--){
                 toPrint.add(0, tmp.get(i));
             }
+            
+            //go through the list of neighbor nodes
             for (Node node : start.getNeighbors()){
-                boolean[] newVisited = new boolean[nodes.size()];
+                boolean[] newVisited = new boolean[nodes.size()]; //we want a new visited array everytime
                 for (int i = 0; i < v.length; i++){
                     newVisited[i] = v[i];
                 }
-                if(newVisited[node.getIndex()] == false){
-                    newVisited[node.getIndex()] = true;
+                if(newVisited[node.getIndex()] == false){ //this might be unnecessary, but currently works with it - so didn't bother taking it out
+                    newVisited[node.getIndex()] = true; //basically the same as DFS
                     Node found = dfsLimit(node, nodes, newVisited, toPrint, limit - 1);
                     if (found != null){
                         return found;
@@ -93,9 +110,10 @@ public class SearchMethods {
     }
     
     public static void iterativeDeeping(Node start, ArrayList<Node> nodes, ArrayList<ArrayList<Node>> toPrint){
-        Node check = null;
-        int index = 0;
-        while(check == null){ 
+        Node check = null; //if dfsLimit found solution it should return a node
+        int index = 0; //keep incrementing the index until iterativeDeeping finds a solution
+        while(check == null){ //dfsLimit continues to return null, we continue to loop
+            //initialized for each new call of dfsLimit
             boolean[] v = new boolean[nodes.size()];
             toPrint = new ArrayList<>();
             ArrayList<Node> tmp = new ArrayList<>();
@@ -129,7 +147,19 @@ public class SearchMethods {
        
     }
     
-    public static void uniformSearch(Node start, ArrayList<Node> nodes, ArrayList<ArrayList<Node>> toPrint){
+    public static void uniformSearch(Node start, ArrayList<Node> nodes, boolean[] v, ArrayList<ArrayList<Node>> toPrint){
+        
+    }
+    
+    public static void greedySearch(Node start, ArrayList<Node> nodes, boolean[] v, ArrayList<ArrayList<Node>> toPrint){
+        
+    }
+    
+    public static void aStar(Node start, ArrayList<Node> nodes, boolean[] v, ArrayList<ArrayList<Node>> toPrint){
+        
+    }
+    
+    public static void beamSearch(Node start, ArrayList<Node> nodes, boolean[] v, ArrayList<ArrayList<Node>> toPrint){
         
     }
     
